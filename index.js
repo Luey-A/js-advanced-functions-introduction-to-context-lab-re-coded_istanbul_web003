69611c6f322e2226a1b57c262a2637d7871d9e37
Loae Monla Ahmed <lueyahmad@gmail.com>
2020-10-29 21:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function createTimeInEvent(obj,stamp){
+    let date = stamp.split(" ");
+    let hour = stamp.split(" ");
+    obj.timeInEvents.push( {
+        type : "TimeIn",
+        hour: parseInt(hour[1]),
+        date: date[0]
+    })
+
+    return obj
+};
+
+
+function createTimeOutEvent(obj,stamp){
+     let date = stamp.split(" ");
+     let hour = stamp.split(" ");
+      obj.timeOutEvents.push( {
+         type : "TimeOut",
+         hour: parseInt(hour[1]),
+         date: date[0]      
+       })
+     return obj
+};
+
+function createEmployeeRecords(arr){
+    let tmp = [];
+     arr.map(x =>{
+       tmp.push(createEmployeeRecord(x))
+     })
+     return tmp;
+};
